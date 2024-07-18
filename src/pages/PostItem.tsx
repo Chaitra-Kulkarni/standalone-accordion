89571ae7bd04d3ccf104a32ccaa12f815bf5aabd
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2024-07-18 15:27:28 +0530

    chore: update styles and add tags in the posts

--- src/pages/PostItem.tsx
+++ src/pages/PostItem.tsx
@@ -3,14 +3,17 @@ import { PostItemProps } from "../types";
 
 const PostItem: React.FC<PostItemProps> = ({ title, body, tags }) => {
   return (
-    <div className="h-[200px] w-[280px] flex flex-col gap-5 p-4 border-2 border-cyan-900 rounded-3xl">
-      <span className="font-medium underline underline-offset-4">{title}</span>
-      <span className="text-ellipsis whitespace-nowrap overflow-hidden">
+    <div className="h-[200px] w-[280px] flex flex-col justify-start gap-5 p-4 border-2 border-cyan-900 rounded-3xl">
+      <div className="flex-1 font-medium underline underline-offset-4">{title}</div>
+      <div className="flex-1 text-ellipsis whitespace-nowrap overflow-hidden">
         {body}
-      </span>
-      <div className="flex align-bottom items-baseline">
-        {tags.map((tag) => (
-          <span className="border border-cyan-100 rounded-xl p-1 mx-1 text-xs">
+      </div>
+      <div className="flex">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="border border-yellow-200 rounded-xl px-2 mr-1 text-xs"
+          >
             {tag}
           </span>
         ))}
